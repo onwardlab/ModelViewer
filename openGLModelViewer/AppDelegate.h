3c42da743caab7d4317665a24d77f215a9a0b697
SpaceCadet <Space@wtref-pc.home>
2012-02-08 23:16:05 -0500

    Initial Commit

---
+++ openGLModelViewer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  openGLModelViewer
+//
+//  Created by Tim Flanagan on 2/8/12.
+//  Copyright (c) 2012 Boredom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ openGLModelViewer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  openGLModelViewer
+//
+//  Created by Tim Flanagan on 2/8/12.
+//  Copyright (c) 2012 Boredom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
